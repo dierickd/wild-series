2833d1a2a2159b729059e618d898e52b79212fd8
DIERICK Dewi <dierickd@outlook.fr>
2020-06-08 09:29:22 +0200

    create select by button option

--- src/Form/ActorType.php
+++ src/Form/ActorType.php
@@ -6,6 +6,7 @@ use App\Entity\Actor;
 use App\Entity\Program;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -16,7 +17,7 @@ class ActorType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name', TextType::class)
+            ->add('name', CheckboxType::class)
             ->add('programs', EntityType::class, [
                 'class' => Program::class,
                 'choice_label' => 'title',
