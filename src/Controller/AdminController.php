516721a74827823372ecce3c34d6f137e6a461fc
DIERICK Dewi <dierickd@outlook.fr>
2020-06-16 22:49:21 +0200

    added user and fixtures

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -4,13 +4,25 @@ namespace App\Controller;
 
 use App\Entity\Category;
 use App\Form\CategoryType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Require ROLE_ADMIN for *every* controller method in this class
+ * @IsGranted("ROLE_ADMIN")
+ *
+ * Class AdminController
+ * @package App\Controller
+ */
 class AdminController extends AbstractController
 {
+
     /**
+     * Require ROLE_ADMIN for *every* controller method in this class
+     * @IsGranted("ROLE_ADMIN")
+     *
      * @Route("/admin", name="admin")
      */
     public function index(): Response
